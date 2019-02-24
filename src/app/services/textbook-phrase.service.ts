059bd736f33d360321dbdd1a19a75097d7f9dc05
趙偉 <zwvista@msn.com>
2019-02-24 12:30:21 +0900

    units

--- src/app/services/textbook-phrase.service.ts
+++ src/app/services/textbook-phrase.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { TextbookPhrase, TextbookPhrases } from '../models/textbook-phrase';
 import { map } from 'rxjs/operators';
+import { partsFrom, unitsFrom } from '../common/common';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +19,12 @@ export class TextbookPhraseService extends BaseService {
     const url = `${this.baseUrl}VTEXTBOOKPHRASES?transform=1&filter=LANGID,eq,${langid}&order[]=TEXTBOOKID&order[]=UNIT&order[]=PART&order[]=SEQNUM`;
     return this.http.get<TextbookPhrases>(url)
       .pipe(
-        map(result => result.VTEXTBOOKPHRASES.map(value => Object.assign(new TextbookPhrase(), value))),
+        map(result => result.VTEXTBOOKPHRASES.map(value => {
+          const v = Object.assign(new TextbookPhrase(), value);
+          v.units = unitsFrom(v.UNITINFO);
+          v.parts = partsFrom(v.PARTS);
+          return v;
+        })),
       );
   }
 }
--- src/app/services/textbook-word.service.ts
+++ src/app/services/textbook-word.service.ts
@@ -4,6 +4,7 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { TextbookWord, TextbookWords } from '../models/textbook-word';
+import { partsFrom, unitsFrom } from '../common/common';
 
 @Injectable({
   providedIn: 'root'
@@ -18,7 +19,12 @@ export class TextbookWordService extends BaseService {
     const url = `${this.baseUrl}VTEXTBOOKWORDS?transform=1&filter=LANGID,eq,${langid}&order[]=TEXTBOOKID&order[]=UNIT&order[]=PART&order[]=SEQNUM`;
     return this.http.get<TextbookWords>(url)
       .pipe(
-        map(result => result.VTEXTBOOKWORDS.map(value => Object.assign(new TextbookWord(), value))),
+        map(result => result.VTEXTBOOKWORDS.map(value => {
+          const v = Object.assign(new TextbookWord(), value);
+          v.units = unitsFrom(v.UNITINFO);
+          v.parts = partsFrom(v.PARTS);
+          return v;
+        })),
       );
   }
 }
