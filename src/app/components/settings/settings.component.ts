0c8ddaea540cb9c7010fce0a2891809c953901ae
趙偉 <zwvista@msn.com>
2019-02-28 01:58:02 +0900

    unit

--- src/app/components/settings/settings.component.ts
+++ src/app/components/settings/settings.component.ts
@@ -21,8 +21,18 @@ export class SettingsComponent implements OnInit {
   get partTo() {
     return this.settingsService.parts[this.settingsService.USPARTTO - 1];
   }
+  get toTypeIsUnit() {
+    return this.toType === 0;
+  }
+  get toTypeIsPart() {
+    return this.toType === 1;
+  }
+  get toTypeIsTo() {
+    return this.toType === 2;
+  }
 
-  unitPartTo: boolean;
+  toTypes = ['Unit', 'Part', 'To'].map((v, i) => ({value: i, label: v}));
+  toType = 0;
 
   constructor(private settingsService: SettingsService) { }
 
@@ -52,98 +62,115 @@ export class SettingsComponent implements OnInit {
   }
 
   onUnitFromChange(index) {
-    this.settingsService.USUNITFROM = index + 1;
-    this.settingsService.updateUnitFrom()
-      .subscribe(_ => {
-        if (!this.unitPartTo || this.settingsService.isInvalidUnitPart) {
-          this.updateUnitPartTo();
-        }
-      });
+    if (!this.updateUnitFrom(index + 1)) return;
+    if (this.toType === 0)
+      this.updateSingleUnit();
+    else if (this.toType === 1 || this.settingsService.isInvalidUnitPart)
+      this.updateUnitPartTo();
   }
 
   onPartFromChange(index) {
-    this.settingsService.USPARTFROM = index + 1;
-    this.settingsService.updatePartFrom()
-      .subscribe(_ => {
-        if (!this.unitPartTo || this.settingsService.isInvalidUnitPart) {
-          this.updateUnitPartTo();
-        }
-      });
+    if (!this.updatePartFrom(index + 1)) return;
+    if (this.toType === 1 || this.settingsService.isInvalidUnitPart)
+      this.updateUnitPartTo();
+  }
+
+  onToTypeChange(index) {
+    if (index === 0)
+      this.updateSingleUnit();
+    else if (index === 1)
+      this.updateUnitPartTo();
   }
 
   onUnitToChange(index) {
-    this.settingsService.USUNITTO = index + 1;
-    this.settingsService.updateUnitTo()
-      .subscribe(_ => {
-        if (this.settingsService.isInvalidUnitPart) {
-          this.updateUnitPartFrom();
-        }
-      });
+    if (!this.updateUnitTo(index + 1)) return;
+    if (this.toType === 1 || this.settingsService.isInvalidUnitPart)
+      this.updateUnitPartFrom();
   }
 
   onPartToChange(index) {
-    this.settingsService.USPARTTO = index + 1;
-    this.settingsService.updatePartTo()
-      .subscribe(_ => {
-        if (this.settingsService.isInvalidUnitPart) {
-          this.updateUnitPartFrom();
-        }
-      });
+    if (!this.updatePartTo(index + 1)) return;
+    if (this.toType === 1 || this.settingsService.isInvalidUnitPart)
+      this.updateUnitPartFrom();
   }
 
   updateTextbook() {
-    this.unitPartTo = !this.settingsService.isSingleUnitPart;
-  }
-
-  updateUnitPartFrom() {
-    if (this.settingsService.USUNITFROM !== this.settingsService.USUNITTO) {
-      this.settingsService.USUNITFROM = this.settingsService.USUNITTO;
-      this.settingsService.updateUnitFrom().subscribe();
-    }
-    if (this.settingsService.USPARTFROM !== this.settingsService.USPARTTO) {
-      this.settingsService.USPARTFROM = this.settingsService.USPARTTO;
-      this.settingsService.updatePartFrom().subscribe();
-    }
-  }
-
-  updateUnitPartTo() {
-    if (this.settingsService.USUNITTO !== this.settingsService.USUNITFROM) {
-      this.settingsService.USUNITTO = this.settingsService.USUNITFROM;
-      this.settingsService.updateUnitTo().subscribe();
-    }
-    if (this.settingsService.USPARTTO !== this.settingsService.USPARTFROM) {
-      this.settingsService.USPARTTO = this.settingsService.USPARTFROM;
-      this.settingsService.updatePartTo().subscribe();
-    }
+    this.toType = this.settingsService.isSingleUnitPart ? 1 : this.settingsService.isSingleUnit ? 0 : 2;
   }
 
   previousUnitPart() {
-    if (this.settingsService.USPARTFROM > 1) {
-      this.settingsService.USPARTFROM--;
+    if (this.toType === 0) {
+      if (this.settingsService.USUNITFROM > 1) {
+        this.updateUnitFrom(this.settingsService.USUNITFROM - 1);
+        this.updateUnitTo(this.settingsService.USUNITFROM);
+      }
+    } else if (this.settingsService.USPARTFROM > 1) {
+      this.updatePartFrom(this.settingsService.USPARTFROM - 1);
       this.updateUnitPartTo();
-      this.settingsService.updatePartFrom().subscribe();
     } else if (this.settingsService.USUNITFROM > 1) {
-      this.settingsService.USUNITFROM--;
-      this.settingsService.USPARTFROM = this.settingsService.parts.length;
+      this.updateUnitFrom(this.settingsService.USUNITFROM - 1);
+      this.updatePartFrom(this.settingsService.parts.length);
       this.updateUnitPartTo();
-      this.settingsService.updateUnitFrom().pipe(
-        concatMap(_ => this.settingsService.updatePartFrom())
-      ).subscribe();
     }
   }
 
   nextUnitPart() {
-    if (this.settingsService.USPARTFROM < this.settingsService.parts.length) {
-      this.settingsService.USPARTFROM++;
+    if (this.toType === 0) {
+      if (this.settingsService.USUNITFROM < this.settingsService.units.length) {
+        this.updateUnitFrom(this.settingsService.USUNITFROM + 1);
+        this.updateUnitTo(this.settingsService.USUNITFROM);
+      }
+    } else if (this.settingsService.USPARTFROM < this.settingsService.parts.length) {
+      this.updatePartFrom(this.settingsService.USPARTFROM + 1);
       this.updateUnitPartTo();
-      this.settingsService.updatePartFrom().subscribe();
     } else if (this.settingsService.USUNITFROM < this.settingsService.units.length) {
-      this.settingsService.USUNITFROM++;
-      this.settingsService.USPARTFROM = 1;
+      this.updateUnitFrom(this.settingsService.USUNITFROM + 1);
+      this.updatePartFrom(1);
       this.updateUnitPartTo();
-      this.settingsService.updateUnitFrom().pipe(
-        concatMap(_ => this.settingsService.updatePartFrom())
-      ).subscribe();
     }
+  }
+
+  updateUnitPartFrom() {
+    this.updateUnitFrom(this.settingsService.USUNITTO);
+    this.updatePartFrom(this.settingsService.USPARTTO);
+  }
+
+  updateUnitPartTo() {
+    this.updateUnitTo(this.settingsService.USUNITFROM);
+    this.updatePartTo(this.settingsService.USPARTFROM);
+  }
+
+  updateSingleUnit() {
+    this.updateUnitTo(this.settingsService.USUNITFROM);
+    this.updatePartFrom(1);
+    this.updatePartTo(this.settingsService.parts.length);
+  }
+
+  updateUnitFrom(v: number): boolean {
+    if (this.settingsService.USUNITFROM === v) return false;
+    this.settingsService.USUNITFROM = v;
+    this.settingsService.updateUnitFrom().subscribe();
+    return true;
+  }
+
+  updatePartFrom(v: number): boolean {
+    if (this.settingsService.USPARTFROM === v) return false;
+    this.settingsService.USPARTFROM = v;
+    this.settingsService.updatePartFrom().subscribe();
+    return true;
+  }
+
+  updateUnitTo(v: number): boolean {
+    if (this.settingsService.USUNITTO === v) return false;
+    this.settingsService.USUNITTO = v;
+    this.settingsService.updateUnitTo().subscribe();
+    return true;
+  }
+
+  updatePartTo(v: number): boolean {
+    if (this.settingsService.USPARTTO === v) return false;
+    this.settingsService.USPARTTO = v;
+    this.settingsService.updatePartTo().subscribe();
+    return true;
   }
 }
