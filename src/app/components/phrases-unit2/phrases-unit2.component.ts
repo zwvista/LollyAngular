00f42076a735fdac64ab38bd94532c83b53e5710
趙偉 <zwvista@msn.com>
2019-04-24 08:37:19 +0900

    row

--- src/app/components/phrases-unit2/phrases-unit2.component.ts
+++ src/app/components/phrases-unit2/phrases-unit2.component.ts
@@ -1,6 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { PhrasesUnitService } from '../../view-models/phrases-unit.service';
 import { SettingsService } from '../../view-models/settings.service';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { MUnitWord } from '../../models/unit-word';
+import { MatTable } from '@angular/material';
+import { googleString } from '../../common/common';
 
 @Component({
   selector: 'app-phrases-unit2',
@@ -8,8 +12,9 @@ import { SettingsService } from '../../view-models/settings.service';
   styleUrls: ['./phrases-unit2.component.css']
 })
 export class PhrasesUnit2Component implements OnInit {
+  @ViewChild('table') table: MatTable<MUnitWord>;
 
-  displayedColumns: string[] = ['ID', 'UNIT', 'PART', 'SEQNUM', 'PHRASEID', 'PHRASE', 'TRANSLATION', 'ACTION'];
+  displayedColumns: string[] = ['position', 'ID', 'UNIT', 'PART', 'SEQNUM', 'PHRASEID', 'PHRASE', 'TRANSLATION', 'ACTION'];
 
   constructor(private phrasesUnitService: PhrasesUnitService,
               private settingsService: SettingsService) { }
@@ -20,6 +25,21 @@ export class PhrasesUnit2Component implements OnInit {
 
   onRefresh() {
     this.phrasesUnitService.getDataInTextbook().subscribe();
+  }
+
+  dropTable(event: CdkDragDrop<MUnitWord[]>) {
+    const prevIndex = this.phrasesUnitService.unitPhrases.findIndex((d) => d === event.item.data);
+    moveItemInArray(this.phrasesUnitService.unitPhrases, prevIndex, event.currentIndex);
+    this.table.renderRows();
+  }
+
+  deletePhrase(index: number) {
+    const o = this.phrasesUnitService.unitPhrases[index];
+    this.phrasesUnitService.delete(o);
+  }
+
+  googlePhrase(phrase: string) {
+    googleString(phrase);
   }
 
 }
