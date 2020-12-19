d8aaa2416f36d9485419438d29f13995b5033970
趙偉 <zwvista@msn.com>
2020-12-19 23:44:23 +0900

    patterns 5

--- src/app/components/patterns-detail/patterns-detail.component.ts
+++ src/app/components/patterns-detail/patterns-detail.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { MLangPhrase } from '../../models/wpp/lang-phrase';
+import { SettingsService } from '../../view-models/settings.service';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { PatternsService } from '../../view-models/patterns.service';
+import { MPattern } from '../../models/wpp/pattern';
 
 @Component({
   selector: 'app-patterns-detail',
@@ -7,9 +13,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PatternsDetailComponent implements OnInit {
 
-  constructor() { }
+  item: MPattern;
 
-  ngOnInit(): void {
+  constructor(private patternsService: PatternsService,
+              private settingsService: SettingsService,
+              private route: ActivatedRoute,
+              private location: Location
+  ) { }
+
+  ngOnInit() {
+    const id = +this.route.snapshot.paramMap.get('id');
+    const o = this.patternsService.patterns.find(value => value.ID === id);
+    this.item = o ? {...o} as MPattern : this.patternsService.newPattern();
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    this.item.PATTERN = this.settingsService.autoCorrectInput(this.item.PATTERN);
+    if (this.item.ID) {
+      this.patternsService.update(this.item).subscribe(_ => this.goBack());
+    } else {
+      this.patternsService.create(this.item).subscribe(_ => this.goBack());
+    }
   }
 
 }
--- src/app/components/patterns2/patterns2.component.ts
+++ src/app/components/patterns2/patterns2.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AppService } from '../../view-models/app.service';
+import { SettingsService } from '../../view-models/settings.service';
+import { googleString } from '../../common/common';
+import { PatternsService } from '../../view-models/patterns.service';
+import { MPattern } from '../../models/wpp/pattern';
 
 @Component({
   selector: 'app-patterns2',
@@ -7,9 +12,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Patterns2Component implements OnInit {
 
-  constructor() { }
+  displayedColumns: string[] = ['ID', 'PATTERN', 'NOTE', 'TAGS', 'ACTION'];
 
-  ngOnInit(): void {
+  rows = 0;
+  page = 1;
+  filter: string;
+  filterType = 0;
+
+  constructor(private appService: AppService,
+              public patternsService: PatternsService,
+              public settingsService: SettingsService) { }
+
+  ngOnInit() {
+    this.appService.initializeObject.subscribe(_ => {
+      this.rows = this.settingsService.USROWSPERPAGE;
+      this.onRefresh();
+    });
+  }
+
+  paginate(event) {
+    this.rows = event.pageSize;
+    this.page = event.pageIndex + 1;
+    this.onRefresh();
+  }
+
+  onRefresh() {
+    this.patternsService.getData(this.page, this.rows, this.filter, this.filterType).subscribe();
+  }
+
+  deletePattern(id: number) {
+    this.patternsService.delete(id);
+  }
+
+  googlePattern(phrase: string) {
+    googleString(phrase);
   }
 
 }
