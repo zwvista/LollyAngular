f0d1737710f92cfa8450fff9a26d3d4fbb23fc7b
趙偉 <zwvista@msn.com>
2025-09-14 02:57:39 +0900

    primeui

---
+++ src/app/app.config.ts
@@ -0,0 +1,15 @@
+import { ApplicationConfig } from '@angular/core';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { providePrimeNG } from 'primeng/config';
+import Aura from '@primeuix/themes/dist/aura';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideAnimationsAsync(),
+    providePrimeNG({
+      theme: {
+        preset: Aura
+      }
+    })
+  ]
+};
